85436dcf2700ef5d23127ad095659e5eb8de030f
jonaszubietaleon <jonas.zubieta@vallegrande.edu.pe>
2024-07-12 08:27:14 -0500

    Initial proyecto

---
+++ src/main/java/pe/edu/vallegrande/GenerativeAI/GenerativeAiApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.GenerativeAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerativeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerativeAiApplication.class, args);
+	}
+
+}
